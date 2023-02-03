d25cf41d6a39009a1a8646546476aa40589efdf8
Chad Bustard <bustardchad@gmail.com>
2023-02-03 08:39:20 -0800

    now output full npy files for each field

--- fromHDF5_to_fits.py
+++ fromHDF5_to_fits.py
@@ -15,17 +15,19 @@ from skimage import exposure
 #      fileName = "cr.out1.00040.athdf"
 #
 # Output: Writes a FITS file named turb.fits to file directory with density, B^2, and v_A fields
-def makeFits(fileDir,fileName):
+
+
+def makeFits(fileDir, fileName):
     # load in file with yt
     ds = yt.load(fileDir+fileName)
 
-    grid = ds.r[::512j,::512j,::512j]
+    grid = ds.r[::512j, ::512j, ::512j]
 
     fid_grid = grid.to_fits_data(
-        fields=[("gas", "density"), ("gas", "magnetic_energy_density"), ("gas","alfven_speed")], length_unit=None)
+       # fields=[("gas", "density"), ("gas", "magnetic_energy_density"), ("gas", "alfven_speed")], length_unit=None)
+        fields=[("athena_pp","Ec"), ("gas", "density"), ("gas", "magnetic_energy_density"), ("gas", "alfven_speed")], length_unit=None)
 
-    fid_grid.writeto(fileDir+"turb_full.fits",overwrite=True)
-
+    fid_grid.writeto(fileDir+"turb_full.fits", overwrite=True)
 
     # test that the file wrote and has the correct stuff in it
     image_data = fits.open(fileDir+'turb_full.fits')
@@ -35,17 +37,14 @@ def makeFits(fileDir,fileName):
     print(image_data["density"].header)
 
 
-
 # do this for all files I want
 
-fileDir_arr = ['Files/MHD_beta1/']
-fileName_arr = ['cr.out1.00040.athdf']
+fileDir_arr = ['Files/CR_Diff_Fiducial_beta10/']
+fileName_arr = ['cr.out1.00038.athdf']
 
 i = 0
 for fileDir in fileDir_arr:
     fileName = fileName_arr[i]
     print("Extracting: " + fileDir + fileName)
-    makeFits(fileDir,fileName)
-    i+=1
-
-
+    makeFits(fileDir, fileName)
+    i += 1
--- main.py
+++ main.py
@@ -8,96 +8,119 @@ import cmasher as cmr
 
 # Function adapted from Peek and Burkhart 2019 (https://zenodo.org/record/2658177#.Y9fvEOzMJ24)
 #
-# Requires: fileDir, fitsFileName (turb_full.fits, unless something gets changed), killPwr flag
+# Requires: fileDir -- file directory
+#           fitsFileName -- file name
+#           field -- e.g. 'density'
+#           killPwr -- default is False
 #
-# Output: Creates and populates augmented data cube for a given FITS file following the steps from Peek     and Burkhart 2019:
-#   Create slices in the y-z plane (perpendicular to original B-field direction)
-#   Extract nSlice=16 128 x 128 slices in each plane
-#   Use periodic BCs to augment data -- shift 64 cells in y and z directions and redo slice extraction,     add to dataset
+# Output: Creates and populates augmented data cube for a given FITS file
+#         following the steps from Peek and Burkhart 2019:
 #
-#   Repeat for other snapshots, simulations, etc.
-def preProcessing(fileDir,fileName,field,nx,killPwr=False):
-    fulldata = fits.open(fileDir+'turb_full.fits')
-    data = fulldata[field].data
-    # why multiply by 255 here??
-    data = (data- np.nanmin(data))/(np.nanmax(data)- np.nanmin(data))*255.0
-    data[~np.isfinite(data)] = 0.
+# Steps: Create slices in the y-z plane (perpendicular to original B-field direction)
+#        Extract nSlice=16 128 x 128 slices in each plane
+#        Use periodic BCs to augment data -- shift 64 cells in y and z directions and redo slice extraction
+#        Repeat for other snapshots, simulations, etc.
 
-    imsize,nsh = 128, 2  # hardcoded imagize size, striding, and number of images across a cube
-    sh = imsize/nsh # stride size
+
+def preProcessing(fileDir, field, killPwr=False):
+    # access data from FITS file
+    fulldata = fits.open('Files/'+fileDir + '/turb_full.fits')
+
+    # data is logarithmic, so take the log
+    data = np.log10(fulldata[field].data)
+
+    # why multiply by 255 here??
+    data = (data - np.nanmin(data)) / \
+        (np.nanmax(data) - np.nanmin(data)) * 255.0
+
+    # clean bad values (if any)
+    data[~np.isfinite(data)] = 0.
+    newdata = data
+
+    # hardcoded image size, # of images in each direction, striding (for further augmentation)
+    imsize, nx, nsh = 128, 4, 2
+    sh = imsize / nsh  # stride size
     dsh = data.shape
-    print(dsh)
-    newdata = np.log10(data)
-    cube = np.zeros([imsize, imsize, int(dsh[2]*nsh*nsh*nx*nx)])
+
+
+    # create data cube that will hold all slices
+    cube = np.zeros([imsize, imsize, int(dsh[2] * nsh * nsh * nx * nx)])
     iterct = 0
-    for s in np.arange(dsh[0]):
-        if killPwr:
-            img = data[s, :, :] # slice in y-z plane
-            fftsl= np.fft.fft2(img)
-            normalized = fftsl/np.abs(fftsl)
+    if killPwr:
+        for s in np.arange(dsh[0]):
+            # slice in y-z plane
+            img = data[s, :, :]
+
+            # take 2D fft and normalize (kill the power spectrum)
+            fftsl = np.fft.fft2(img)
+            normalized = fftsl / np.abs(fftsl)
             newimg = np.real(np.fft.ifft2(normalized))
             newdata[s, :, :] = newimg
-    for s in tqdm(np.arange(dsh[2])):
-        for r in np.arange(nsh):
-            for q in np.arange(nsh):
-                fullimg = np.roll(np.roll(newdata[s, :, :], int(r*sh+s), axis=0), int(q*sh+s), axis=1)
-                for i in np.arange(nx):
-                     for j in np.arange(nx):
-                        toimg = fullimg[int(i*imsize):int((i+1)*imsize), int(j*imsize):int((j+1)*imsize)]
-                        exeq = exposure.equalize_hist(toimg) # equalize histogram
-                        cube[:, :, iterct] = exeq
-                        #plt.imshow(toimg)
-                        #print(np.max(toimg))
-                       # exeq = exeq.reshape([imsize, imsize, 1])*np.ones([imsize, imsize, 3])*255
-                       # img = Image.fromarray(exeq.astype('int8'), mode='RGB')
-                        iterct+=1
-    return cube # note: contains all fields, i.e. density, B^2, v_A, etc.
 
+    for s in tqdm(np.arange(dsh[0])):
+        for r in np.arange(nsh):  # two loops that shift the image (augmentation)
+            for q in np.arange(nsh):
+                fullimg = np.roll(np.roll(newdata[s, :, :], int(
+                    r * sh + s), axis=0), int(q * sh + s), axis=1)
+                for i in np.arange(nx):  # two loops that scan acros the 16 128 x 128 images
+                    for j in np.arange(nx):
+                        toimg = fullimg[int(
+                            i * imsize):int((i + 1) * imsize), int(j * imsize):int((j + 1) * imsize)]
+
+                        # equalize histogram
+                        exeq = exposure.equalize_hist(
+                            toimg)
+
+                        # add slice to data cube
+                        cube[:, :, iterct] = exeq
+                        iterct += 1
+    return cube
+
+
+# check a few random slices to make sure they look alright
 def plotRandomSlices(data):
-    # check a few random slices to make sure they look alright
     np.random.seed(0)
-    random_indices = np.floor(np.random.random(3)*2048)
+    random_indices = np.floor(np.random.random(3) * 2048)
     plt.figure(figsize=[16, 8])
     for i in range(3):
-        plt.subplot(1,3,i+1)
+        plt.subplot(1, 3, i + 1)
         plt.imshow(data[:, :, int(random_indices[int(i)])], cmap='cmr.eclipse')
         plt.axis('off')
     plt.savefig('example_slices.pdf')
 
 
-
-# Load in already created FITS files (using makeFits function in fromHDF5_to_cube.py)
-fileDirMHD_arr = ['Files/MHD_beta1/']
-fileNameMHD_arr = ['cr.out1.00040.athdf']
+# Load in already created FITS files (using makeFits function in
+# fromHDF5_to_cube.py)
+fileDirMHD_arr = ['MHD_beta1','MHD_beta10','MHD_beta100']
+field_list = ['density','magnetic_energy_density','alfven_speed']
 
 # also for sims with CRs, etc.
-#fileDirC_arr = ['Files/MHD_beta1/']
-#fileNameMHD_arr = ['cr.out1.00040.athdf']
+# fileDirC_arr = ['Files/MHD_beta1/']
 
 
 # Start with MHD simulations
-nx = 1
+killPwr = False
 
-i = 0
 for fileDir in fileDirMHD_arr:
-    fileName = fileNameMHD_arr[i]
-    killPwr = False
-
+    fulldata = fits.open('Files/' + fileDir + '/turb_full.fits')
     # preprocessing steps
-    field = 'density'
-    full_x = preProcessing(fileDir,fileName,field,nx,killPwr)
+    sim_name = fileDir
+    for field in field_list:
+        full_x = preProcessing(fileDir, field, killPwr)
 
-    # plot random slices
-    plotRandomSlices(full_x)
+        # plot random slices
+        # plotRandomSlices(full_x)
 
 
+        # save full files (not broken into training, validation, and test sets yet)
+        np.save('Files/'+fileDir + f"/data_{sim_name}_{field}.npy", full_x)
+
+        # Save the following for sklearn and PyTorch to do
+"""
     # split into training and test sets, save as .npy files
     train_x = full_x[0:96, :, :]
-    np.save('train_MHD_00040.npy', train_x)  # npy format is faster to read than csv, txt, etc.
+    np.save(f"train_{sim_name}_{field}.npy", train_x)
 
-    test_x = full_x[(128-16-8):-8, :, :]
-    np.save('test_MHD_00040.npy', test_x)  # npy format is faster to read than csv, txt, etc.
-
-    i+=1
-
-
+    test_x = full_x[(128 - 16 - 8):-8, :, :]
+    np.save(fileDir + f"test_{sim_name}_{field}.npy", test_x)
+"""
