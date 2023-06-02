1dde3841c13d16e4d5afc0a7f20428d55de3f865
Chad Bustard <bustardchad@gmail.com>
2023-06-02 10:06:05 -0700

    Corrected path to csv file

--- utilities/unet_setup.py
+++ utilities/unet_setup.py
@@ -119,15 +119,15 @@ def fit(config, model, loss_func, opt, train_dl, valid_dl, alpha = 0.2):
         loss_arr_train.append(train_loss)
         loss_arr_val.append(val_loss)
 
-        df['Epoch'].add(epoch)
-        df['Train_Loss'].add(train_loss)
-        df['Validation_Loss'].add(val_loss)
+        df['Epoch'].add(epoch.cpu())
+        df['Train_Loss'].add(train_loss.cpu())
+        df['Validation_Loss'].add(val_loss.cpu())
 
         if config.use_ssim:
-          struc_arr_train.append(train_struc)
-          struc_arr_val.append(val_struc)
-          df['Train_SSIM'].add(train_struc)
-          df['Validation_SSIM'].add(val_struc)
+          struc_arr_train.append(train_struc.cpu())
+          struc_arr_val.append(val_struc.cpu())
+          df['Train_SSIM'].add(train_struc.cpu())
+          df['Validation_SSIM'].add(val_struc.cpu())
 
 
         # Print out what's happening every epoch
